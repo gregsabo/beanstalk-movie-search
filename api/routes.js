da755ef104e5069e59f77aeced6a37541007008e
Greg Sabo <gregsabo@gmail.com>
2016-02-03 07:51:17 -0800

    Endpoint for fetching movie

--- api/routes.js
+++ api/routes.js
@@ -5,6 +5,7 @@ import request from 'superagent';
 const apikey = process.env.ROTTEN_TOMATOES_API_KEY;
 assert(apikey, 'Application needs a rotten tomatoes API key.');
 const ROTTEN_TOMATOES_SEARCH_URL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json';
+const ROTTEN_TOMATES_FETCH_URL = 'http://api.rottentomatoes.com/api/public/v1.0/movies/'; // + ID.json;
 
 const router = new express.Router();
 export default router;
@@ -13,5 +14,12 @@ router.get('/search', async ({query: {q, page}}, res) => {
   const response = await request.get(
     ROTTEN_TOMATOES_SEARCH_URL
   ).query({q, page, apikey});
+  res.send(JSON.parse(response.text));
+});
+
+router.get('/movies/:id', async ({params: {id}}, res) => {
+  const response = await request.get(
+    `${ROTTEN_TOMATES_FETCH_URL}/${id}.json`
+  ).query({apikey});
   res.send(JSON.parse(response.text));
 });
